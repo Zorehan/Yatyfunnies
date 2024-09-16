c6b75e68e14875fb7ff8519cd6aea2a12e190361
chris-ebbesen <92531154+chris-ebbesen@users.noreply.github.com>
2024-09-16 09:34:16 +0200

    Update Logik.js
    Sum og totalsum af terningeslag er oprettet

--- Logik.js
+++ Logik.js
@@ -1,13 +1,13 @@
 
 
-//Læg tallene sammen
+
 
 //Ruller terningen  
 function rollDie() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
-//rulle 6 terninger 
+//Ruller 6 terninger 
 function rollDice(numOfDice = 6) {
     let diceResults = [];
     for (let i = 0; i < numOfDice; i++) {
@@ -18,10 +18,50 @@ function rollDice(numOfDice = 6) {
     return diceResults;
 }
 
+//Lægger tallene sammen
+function sumDiceResults(diceResults) {
+    return diceResults.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+//Udregner totalsummen af alle slag 
+let totalSum = sumDiceResults(diceRoll); 
+console.log("Sum of dice rolls:", totalSum);
 
 
 
+//eventhandler der skal rulle terningerne 
 
 
+//---------------------------------------------------------------------------------
+//Select hvilken boks slaget skal gemmes i:
 
-//Select hvilken boks slaget skal gemmes i 
+
+//1-er
+
+//2-er
+
+//3-er
+
+//4-er
+
+//5-er
+
+//6-er
+
+//Et par
+
+//To par
+
+//tre par
+
+//fire par
+
+//fuldt hus
+
+//lille straight
+
+//stor straight
+
+//chance
+
+//yatzy 
